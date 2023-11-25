14b23397bc7dca3995cee285a2601769733d859e
mithun241105 <mithun24112005@gmail.com>
2023-11-25 23:59:13 +0530

    my python project

--- waterrem.py
+++ waterrem.py
@@ -4,7 +4,7 @@ while True:
     notification.notify(
         title="***please drink water now!!!***",
         message="The National Academies of Science, Engineering and Medicine recommends the following for daily fluid intake: 125 ounces (3.7 liters) for men.",
-        app_icon="D:\python\waterbottle.ico",
+        app_icon="D:\python\icon.ico", 
         timeout=10
     )
     time.sleep(60*60)
