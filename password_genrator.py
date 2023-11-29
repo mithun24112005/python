7aa2b53125b73d8a461dca7bd401c3c59eddb7e5
mithun241105 <mithun24112005@gmail.com>
2023-11-29 23:11:00 +0530

    my python project

--- password_genrator.py
+++ password_genrator.py
@@ -9,23 +9,25 @@ spchar = string.punctuation
 s = []
 pw = ""
 password = []
+op=[0,1]
 low = int(input("do u want lower case letters 0 or 1: "))
 upp = int(input("do u want upper case letters 0 or 1: "))
 num = int(input("do u want numbers in password 0 or 1: "))
 spch = int(input("do u want special charecters 0 or 1: "))
-if low == 1:
-    s.extend(lowle)
-if upp == 1:
-    s.extend(uple)
-if num == 1:
-    s.extend(digits)
-if spch == 1:
-    s.extend(spchar)
 
-for i in range(len):
-    password.extend(random.choice(s))
-
-for i in password:
-    pw += i
+if(low or upp or num or spch != 0):
+    if low == 1:
+        s.extend(lowle)
+    if upp == 1:
+        s.extend(uple)
+    if num == 1:
+        s.extend(digits)
+    if spch == 1:
+        s.extend(spchar)
+    for i in range(len):
+        pw += random.choice(s)
+else:
+    print("invalid input.")
+    quit()
 
 print(pw)
