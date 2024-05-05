0b6d5188297b95fcbc474618ecfd82aadbeb55fb
mithun241105 <mithun24112005@gmail.com>
2024-05-05 16:47:58 +0530

    my pyhton prject

---
+++ qr_code.py
@@ -0,0 +1,9 @@
+import qrcode
+import qrcode.constants 
+# img=qr.make("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl&index=1")
+from PIL import Image
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4,)
+qr.add_data("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl&index=1")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="blue")
+img.save("qrcode.jpg") 
