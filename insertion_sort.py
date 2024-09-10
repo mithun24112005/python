9dd47f127b22037554924471f8d26f9f0dd5e291
mithun241105 <mithun24112005@gmail.com>
2024-09-10 23:52:09 +0530

    python practice problems

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,28 @@
-# insertion sort
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        j = i
-        while arr[j - 1] > arr[j] and j > 0:
-            arr[j - 1], arr[j] = arr[j], arr[j - 1]  # exchanging values of j-1 and j
-            j -= 1
+# # insertion sort
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         j = i
+#         while arr[j - 1] > arr[j] and j > 0:
+#             arr[j - 1], arr[j] = arr[j], arr[j - 1]  # exchanging values of j-1 and j
+#             j -= 1
+#     return arr
+
+
+# arr = [2, 34, 53, 78, 96, 45, 36, 12]
+# print(insertion_sort(arr))
+
+def insertion(arr):
+    n=len(arr)
+
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1],arr[j]=arr[j],arr[j-1]  #exchanging values
+            j-=1
+            arr[j+1]=key
     return arr
 
+arr=[12,23,43,53,24,5,15,6,8,90]
+print(insertion(arr))
 
-arr = [2, 34, 53, 78, 96, 45, 36, 12]
-print(insertion_sort(arr))
--- selection_sort.py
+++ selection_sort.py
@@ -1,13 +1,27 @@
 # selection sort
-def selectionsort(l):
-    for start in range(len(l)):
-        for i in range(start, len(l)):
-            minpos = start
-            if l[i] < l[minpos]:
-                minpos = i
-                (l[start], l[minpos]) = (l[minpos], l[start])
-    return l
+# def selectionsort(l):
+#     for start in range(len(l)):
+#         for i in range(start, len(l)):
+#             minpos = start
+#             if l[i] < l[minpos]:
+#                 minpos = i
+#                 (l[start], l[minpos]) = (l[minpos], l[start])
+#     return l
 
 
-l = list(range(1000, 0, -1))
-print(selectionsort(l))
+# l = list(range(1000, 0, -1))
+# print(selectionsort(l))
+
+def selection(arr):
+    n=len(arr)
+    for i in range(n-1):
+        mini=1
+        for j in range(i+1,n):
+            if arr[j]<arr[mini]:
+# find least index with j and assign mini to j and swap it with index i
+                mini=j
+                arr[i],arr[mini]=arr[mini],arr[i]
+    return arr
+
+arr=[12,23,43,53,24,5,15,6,8,90]
+print(selection(arr))
